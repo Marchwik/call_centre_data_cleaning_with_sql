2d4fa46186eca9d7c1d18e887e3d9d3a0ca6def6
Mateusz Marchewka <125673125+Marchwik@users.noreply.github.com>
2023-12-03 13:33:40 +0000

    Update and rename sql data cleaning call centre to cleaning_data.sql

--- cleaning_data.sql
+++ cleaning_data.sql
@@ -1,6 +1,35 @@
--- the table is too big to view all of it, let's see the first 20 rows
+-- first off, before importing the data I create a database to load the data into it
+-- or just load the table into an existing database 
+
+CREATE DATABASE call_centre;
+
+
+-- starting working on new database
 
 USE call_centre;
+
+
+-- Create a table that will contain the imported csv file 
+
+CREATE TABLE calls (
+	ID CHAR(50),
+	cust_name CHAR (50),
+	sentiment CHAR (20),
+	csat_score INT,
+	call_timestamp CHAR (10),
+	reason CHAR (20),
+	city CHAR (20),
+	state CHAR (20),
+	channel CHAR (20),
+	response_time CHAR (20),
+	call_duration_minutes INT,
+	call_center CHAR (20)
+);
+
+
+-- Now I used table data import wizard and loaded our data in
+
+-- The table is too big to view all of it, let's see the first 20 rows
 
 SELECT * FROM calls LIMIT 20;
 
